4f3043ef50947f2147b576c86040024efad77b54
Shefeek Najeeb <connectshefeek@gmail.com>
2025-02-07 16:27:41 +0400

    added scripts for LLM model download

---
+++ scripts/download_models.py
@@ -0,0 +1,41 @@
+#! .venv/bin/python
+
+import os
+import requests
+from tqdm import tqdm
+
+
+def download_file(url, filename):
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get("content-length", 0))
+    block_size = 1024  # 1 KB
+
+    with open(filename, "wb") as file, tqdm(
+        desc=filename,
+        total=total_size,
+        unit="iB",
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as progress_bar:
+        for data in response.iter_content(block_size):
+            size = file.write(data)
+            progress_bar.update(size)
+
+
+def ensure_model(model_name, url):
+    model_path = f"./data/{model_name}"
+    if not os.path.exists(model_path):
+        print(f"Downloading {model_name}...")
+        download_file(url, model_path)
+        print(f"{model_name} downloaded successfully.")
+    else:
+        print(f"{model_name} already exists. Skipping download.")
+
+
+# LLM Model
+llm_model = "Qwen2-VL-2B-Instruct-Q6_K.gguf"
+llm_url = os.getenv("LLM_MODEL", "https://huggingface.co/bartowski/Qwen2-VL-2B-Instruct-GGUF/resolve/main/Qwen2-VL-2B-Instruct-Q6_K.gguf")
+
+ensure_model(llm_model, llm_url)
+
+print("All required models have been downloaded.")
